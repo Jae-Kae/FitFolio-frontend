4c704aba2245b9578b19b5a79065dfdc34951e2a
Janette King <janette@haply.co>
2024-03-22 17:21:31 -0400

    first commit

---
+++ src/components/WorkoutDetails.jsx
@@ -0,0 +1,36 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const date = new Date(workout.createdAt).toDateString()
+
+  const handleDelete = async () => {
+    const response = await fetch(`/api/workouts/${workout._id}`, {
+      method: 'DELETE',
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: json })
+    }
+  }
+
+  return (
+    <div className='workout-details'>
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (lbs):</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong> {workout.reps}
+      </p>
+      <p>{date}</p>
+      <span className='material-symbols-outlined' onClick={handleDelete}>
+        delete
+      </span>
+    </div>
+  )
+}
+
+export default WorkoutDetails
